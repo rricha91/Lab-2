6808f4101d7e6f39b58ce95818e2d6083a1153d9
rricha91 <60485491+rricha91@users.noreply.github.com>
2020-02-11 15:53:22 -0500

    Update NumericTypes.java

--- NumericTypes.java
+++ NumericTypes.java
@@ -6,46 +6,43 @@ import java.util.Scanner;
 public class NumericTypes {
 	public static void main (String [] args) {
 		
-		//TASK #2 Create a Scanner object here 
-		//identifier declarations
-		
-		
-		String output; // line of output to print out
-		
-		//Task #2 prompt user to input score1
-		//Task #2 read score1 
-		//Task #2 prompt user to input score2
-		//Task #2 read score2 
-		// Find an arithmetic average
+		Scanner input = new Scanner(System.in);
+		String output; 
+
 		
 		final int NUMBER = 2; // number of scores
-		int score1 = 100; // first test score
-		int score2 = 95; // second test score
+
+		System.out.println("Please enter the first score: "); // prompt user to input score1
+		int score1 = input.nextInt();											 // read score1
+ 
+		System.out.println("Please enter the second score: "); 				 // prompt user to input score2
+		int score2 = input.nextInt();											 // read score2 
 		
-		double average = ((double) score1 + score2) / NUMBER; // arithmetic average
-		
-		
-		output = score1 + " and " + score2 + " have an average of " + average;	
+		double average = ((double) score1 + score2) / NUMBER; 				// Find arithmetic average
+
+		String output = score1 + " and " + score2 + " have an average of " + average;	// line of output to print out
 		System.out.println(output);
 		
-		// Convert Fahrenheit temperatures to Celsius
+// Convert Fahrenheit temperatures to Celsius
 		
-		final double BOILING_IN_F = 212; 					// boiling temperature
+		final double BOILING_IN_F = 212; 												// boiling temperature
 		
-		
-		
-		double fToC = (5.0/9.0) * (BOILING_IN_F - 32.0); 	// temperature in Celsius
+		double fToC = (5.0/9.0) * (BOILING_IN_F - 32.0); 								// temperature in Celsius
 		output = BOILING_IN_F + " in Fahrenheit is " + fToC + " in Celsius.";
 		
 		System.out.println(output);
 		
-		Scanner input = new Scanner(System.in);
-		double inputTempInFar = input.nextDouble();								// declare a variable to hold the user’s temperature
-																				// Then prompt user to input another temperature
-																				// then read their input
+																				/* 
+																				 * declare a variable to hold the userâ€™s temperature
+																				 * Then prompt user to input another temperature
+																				 * then read their input
+																				 */
+		System.out.println("Please enter the tempreture you want in Degrees F: "); 
+		double inputTempInFar = input.nextDouble();								
+																			
 	
-		double fToC = (5.0/9.0) * (inputTempInFar - 32.0); 						// convert the user’s temperature to Celsius
-		output = InputTempInFar + " in Fahrenheit is " + fToC + " in Celsius."; // print the user’s temperature in Celsius
+		fToC = (5.0/9.0) * (inputTempInFar - 32.0); 							// convert the userâ€™s temperature to Celsius
+		output = inputTempInFar + " in Fahrenheit is " + fToC + " in Celsius."; // print the userâ€™s temperature in Celsius
 		
 		System.out.println("Goodbye"); // to show that the program is ended	
 	}
