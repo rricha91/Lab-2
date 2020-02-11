c2e5513155ec5484548028f6725b796211d35c94
rricha91 <60485491+rricha91@users.noreply.github.com>
2020-02-11 16:15:33 -0500

    Update NumericTypes.java

--- NumericTypes.java
+++ NumericTypes.java
@@ -7,43 +7,44 @@ public class NumericTypes {
 	public static void main (String [] args) {
 		
 		Scanner input = new Scanner(System.in);
-		String output; 
 
 		
+// Calculate the average of two scores
 		final int NUMBER = 2; // number of scores
 
-		System.out.println("Please enter the first score: "); // prompt user to input score1
-		int score1 = input.nextInt();											 // read score1
+		System.out.println("Please enter the first score:"); 				// prompt user to input score1
+		int score1 = input.nextInt();										// read score1
  
-		System.out.println("Please enter the second score: "); 				 // prompt user to input score2
-		int score2 = input.nextInt();											 // read score2 
+		System.out.println("\nPlease enter the second score:"); 				// prompt user to input score2
+		int score2 = input.nextInt();										// read score2 
 		
-		double average = ((double) score1 + score2) / NUMBER; 				// Find arithmetic average
+		double average = ((double) score1 + score2) / NUMBER; 						// Find arithmetic average
 
-		String output = score1 + " and " + score2 + " have an average of " + average;	// line of output to print out
+		String output = "\n\t" + score1 + " and " + score2 + " have an average of " + average;	// line of output to print out
 		System.out.println(output);
 		
-// Convert Fahrenheit temperatures to Celsius
 		
+		
+// Convert Fahrenheit temperatures to Celsius
 		final double BOILING_IN_F = 212; 												// boiling temperature
 		
 		double fToC = (5.0/9.0) * (BOILING_IN_F - 32.0); 								// temperature in Celsius
-		output = BOILING_IN_F + " in Fahrenheit is " + fToC + " in Celsius.";
+		output = "\n\tTast 1 Complete!\n\tTask 2: Temprature Conversion\n\n\tConverting Water Boiling point from Fahrenheit to Celsius...\n\t" + BOILING_IN_F + " degrees Fahrenheit is equal to " + fToC + " degrees Celsius.";
 		
 		System.out.println(output);
 		
-																				/* 
-																				 * declare a variable to hold the user’s temperature
-																				 * Then prompt user to input another temperature
-																				 * then read their input
-																				 */
-		System.out.println("Please enter the tempreture you want in Degrees F: "); 
-		double inputTempInFar = input.nextDouble();								
-																			
-	
-		fToC = (5.0/9.0) * (inputTempInFar - 32.0); 							// convert the user’s temperature to Celsius
-		output = inputTempInFar + " in Fahrenheit is " + fToC + " in Celsius."; // print the user’s temperature in Celsius
+																						/* 
+																						 * declare a variable to hold the user’s temperature
+																						 * Then prompt user to input another temperature
+																						 * then read their input
+																						 */
+		System.out.println("\nPlease enter a tempreture in Degrees F:"); 
+		double inputTempInFar = input.nextDouble();																											
 		
-		System.out.println("Goodbye"); // to show that the program is ended	
+		fToC = (5.0/9.0) * (inputTempInFar - 32.0); 											// convert the user’s temperature to Celsius
+		output = "\n\t" + inputTempInFar + " in Fahrenheit is " + fToC + " degrees Celsius."; 	// print the user’s temperature in Celsius
+		System.out.println(output);
+		
+		System.out.println("\n\tGoodbye"); // to show that the program is ended	
 	}
 }
